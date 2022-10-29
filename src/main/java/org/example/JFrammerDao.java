9dc647fa6f5f79782a6c9ac542ee087743d3c91f
rabbitST <biroattila.eu@gmail.com>
2022-10-29 11:41:55 +0200

    Fourth commit

---
+++ src/main/java/org/example/JFrammerDao.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class JFrammerDao {
+    Scanner scanner=new Scanner(System.in);
+    public void printScanner(){
+        System.out.println(scanner.nextLine());
+    }
+}
